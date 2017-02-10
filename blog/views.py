1e0df54bc73326b162aaa6e2c1912716ac2ec757
Wendell Alves <wendellmccaulay@hotmail.com>
2017-02-10 02:05:31 -0200

    Adicionado template post_detail.html
    Template que exibe um post específico

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
+
 def post_list(request):
     posts = Post.objects.filter(data_publicacao__lte=timezone.now()).order_by('data_publicacao')
-    return render(request, 'blog/post_list.html', {'posts' : posts})
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, "blog/post_detail.html", {"post": post})
