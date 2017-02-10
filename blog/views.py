41cb7239fdec0beee4944adbe5c4c2401dbe913f
Wendell Alves <wendellmccaulay@hotmail.com>
2017-02-10 00:44:56 -0200

    Atualizando o template post_list
    Utilizando Django para exibir o conteúdo dinamicamente.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(data_publicacao__lte=timezone.now()).order_by('data_publicacao')
+    return render(request, 'blog/post_list.html', {'posts' : posts})
